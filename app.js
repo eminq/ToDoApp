21492c18670d2bc46e1dbc7241f8da5fdc7b10c9
Emina <eminatalic23@gmail.com>
2023-04-07 15:10:07 +0200

    second-commit

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const ejs = require('ejs');
+const ejsMate = require('ejs-mate'); // this is for layouts partials ..
+const path = require('path');
 const port = 3000;
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('home');
 })
 
 app.listen(port, () => {
